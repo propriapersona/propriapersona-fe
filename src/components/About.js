4dcd628b1d730460ed73de77bf9e4c3eff73a582
Patrick Shushereba <pshushereba@gmail.com>
2020-09-17 20:21:48 -0700

    Added resized SVGs to About page.

--- src/components/About.js
+++ src/components/About.js
@@ -7,23 +7,25 @@ import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import CardMedia from "@material-ui/core/CardMedia";
 import about_profile from "../assets/about_profile.svg";
-import about_status from "../assets/about_status.svg";
+import about_case from "../assets/about_case.svg";
+import about_forms from "../assets/about_forms.svg";
 
 const useStyles = makeStyles((theme) => ({
-  // imgContainer: {
-  //   width: "50%",
-  //   height: "50%",
-  //   marginLeft: "5em",
-  // },
   mainContainer: {
     backgroundColor: "#E3E3E3",
     paddingTop: "25px",
     height: "90vh",
     margin: "auto",
   },
-  // topContainer: {
-  //   margin: "auto",
-  // },
+  profileImg: {
+    paddingTop: "25px",
+  },
+  introHeading: {
+    paddingBottom: "25px",
+  },
+  formTextContainer: {
+    width: "50%",
+  },
 }));
 
 const About = () => {
@@ -32,17 +34,57 @@ const About = () => {
   return (
     <>
       <Nav />
-      <Grid container direction="column" className={classes.mainContainer}>
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        className={classes.mainContainer}
+      >
         <Grid item>
-          <Grid container justify="space-around" alignItems="center">
+          <Grid container justify="space-evenly" alignItems="center">
             <Grid item>
-              <Typography variant="h4">
+              <Typography variant="h4" className={classes.introHeading}>
                 Navigating the court system is hard,
                 <br /> but it doesn't have to be.
               </Typography>
+              <Typography variant="body1">
+                Start by creating a profile, and providing us some basic
+                information about you and your case.
+              </Typography>
             </Grid>
-            <Grid item justify="flex-end">
+            <Grid item justify="center" className={classes.profileImg}>
               <img src={about_profile} alt="profile" />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid container justify="space-evenly" alignItems="center">
+            <Grid item justify="center">
+              <img src={about_forms} alt="forms" />
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" className={classes.formTextContainer}>
+                Once your profile is set up, you can search for different
+                government forms that you could file on your case. With your
+                profile information complete, a lot of the preliminary
+                information can be completed. This will help save you some time
+                in filling out the rest of the forms. You can also save your
+                progress and come back to finish later.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid container justify="space-evenly" alignItems="center">
+            <Grid item>
+              <Typography variant="body1">
+                Keep track of all of the forms that you have filed on your case.
+                If something needs to be amended or changed, you'll have it
+                readily available.
+              </Typography>
+            </Grid>
+            <Grid item>
+              <img src={about_case} alt="case" />
             </Grid>
           </Grid>
         </Grid>
