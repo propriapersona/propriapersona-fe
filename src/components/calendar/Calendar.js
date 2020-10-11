4fce4f6aca2cddc5ea8c994668ba05cd34cec46c
Patrick Shushereba <pshushereba@gmail.com>
2020-10-11 14:57:59 -0700

    Started calendar feature.

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,9 +1,17 @@
 import React from "react";
+import dayjs from "dayjs";
 
 const Calendar = () => {
+  const value = dayjs();
+  const startDay = value.clone().startOf("month").startOf("week");
+  const endDay = value.clone().endOf("month").endOf("week");
+
   return (
     <div>
       <h1>Calendar will go here.</h1>
+      <div>
+        {startDay.format("MM/DD")} - {endDay.format("MM/DD")}
+      </div>
     </div>
   );
 };
