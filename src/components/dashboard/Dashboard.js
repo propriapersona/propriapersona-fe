260c8a07c6b6699fc59d5bea45819bf1947604a7
Patrick Shushereba <pshushereba@gmail.com>
2020-10-27 20:12:18 -0700

    Removed theme code from dashboard.

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Navigator from "./Navigator.js";
 import Content from "./Content.js";
-import {
-  createMuiTheme,
-  ThemeProvider,
-  withStyles,
-} from "@material-ui/core/styles";
+import { ThemeProvider, withStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Hidden from "@material-ui/core/Hidden";
 import Header from "./Header.js";
@@ -17,116 +13,7 @@ import { useParams, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 import { getAccount } from "../../actions/index.js";
 import TaskList from "../tasks/TaskList.js";
-
-let theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: "#63ccff",
-      main: "#009be5",
-      dark: "#006db3",
-    },
-  },
-  typography: {
-    h5: {
-      fontWeight: 500,
-      fontSize: 26,
-      letterSpacing: 0.5,
-    },
-  },
-  shape: {
-    borderRadius: 8,
-  },
-  props: {
-    MuiTab: {
-      disableRipple: true,
-    },
-  },
-  mixins: {
-    toolbar: {
-      minHeight: 48,
-    },
-  },
-});
-
-theme = {
-  ...theme,
-  overrides: {
-    MuiDrawer: {
-      paper: {
-        backgroundColor: "#18202c",
-      },
-    },
-    MuiButton: {
-      label: {
-        textTransform: "none",
-      },
-      contained: {
-        boxShadow: "none",
-        "&:active": {
-          boxShadow: "none",
-        },
-      },
-    },
-    MuiTabs: {
-      root: {
-        marginLeft: theme.spacing(1),
-      },
-      indicator: {
-        height: 3,
-        borderTopLeftRadius: 3,
-        borderTopRightRadius: 3,
-        backgroundColor: theme.palette.common.white,
-      },
-    },
-    MuiTab: {
-      root: {
-        textTransform: "none",
-        margin: "0 16px",
-        minWidth: 0,
-        padding: 0,
-        [theme.breakpoints.up("md")]: {
-          padding: 0,
-          minWidth: 0,
-        },
-      },
-    },
-    MuiIconButton: {
-      root: {
-        padding: theme.spacing(1),
-      },
-    },
-    MuiTooltip: {
-      tooltip: {
-        borderRadius: 4,
-      },
-    },
-    MuiDivider: {
-      root: {
-        backgroundColor: "#404854",
-      },
-    },
-    MuiListItemText: {
-      primary: {
-        fontWeight: theme.typography.fontWeightMedium,
-      },
-    },
-    MuiListItemIcon: {
-      root: {
-        color: "inherit",
-        marginRight: 0,
-        "& svg": {
-          fontSize: 20,
-        },
-      },
-    },
-    MuiAvatar: {
-      root: {
-        width: 32,
-        height: 32,
-      },
-    },
-  },
-};
+import theme from "../../styles/MuiTheme.js";
 
 const drawerWidth = 256;
 
@@ -210,7 +97,3 @@ const mapStateToProps = (state) => {
 export default connect(mapStateToProps, { getAccount })(
   withStyles(styles)(Dashboard)
 );
-
-// For reference:
-// Dashboard Theme - https://github.com/mui-org/material-ui/blob/master/docs/src/pages/premium-themes/paperbase/Navigator.js
-// Live Demo - https://material-ui.com/premium-themes/paperbase/#
